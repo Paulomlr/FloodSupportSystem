0142c29754b1c4ef71b8d0b2e25e62565577a969
Paulo Cesar <paulocesarpinheirodasilva0@gmail.com>
2024-07-03 14:49:03 -0300

    feat: add Shelter entity

---
+++ src/main/java/com/paulocesar/entity/Shelter.java
@@ -0,0 +1,45 @@
+package com.paulocesar.entity;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Getter
+@Setter
+@ToString
+public class Shelter {
+    private Integer id;
+    private String name;
+    private Address address;
+    private String responsible;
+    private String phone;
+    private String email;
+    private Integer capacity;
+    private Double occupationPercentage;
+
+    @Getter(AccessLevel.NONE)
+    @Setter(AccessLevel.NONE)
+    private Map<Class<? extends Item>, List<Item>> itemList;
+
+    public Shelter(String name, Address address, String responsible, String phone, String email, Integer capacity, Double occupationPercentage) {
+        this.name = name;
+        this.address = address;
+        this.responsible = responsible;
+        this.phone = phone;
+        this.email = email;
+        this.capacity = capacity;
+        this.occupationPercentage = occupationPercentage;
+        this.itemList = new HashMap<>();
+    }
+
+    public void addItem(Item item) {
+        Class<? extends Item> itemType = item.getClass();
+        itemList.computeIfAbsent(itemType, k -> new ArrayList<>()).add(item);
+    }
+}
