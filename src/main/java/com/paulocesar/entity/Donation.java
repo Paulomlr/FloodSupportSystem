d5380fe92a012a6dad37cfe79c8f447234d7e1ae
Paulo Cesar <paulocesarpinheirodasilva0@gmail.com>
2024-07-03 15:33:25 -0300

    feat: add Donation entity

---
+++ src/main/java/com/paulocesar/entity/Donation.java
@@ -0,0 +1,24 @@
+package com.paulocesar.entity;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@ToString
+public class Donation {
+
+    @Setter(AccessLevel.NONE)
+    private Integer id;
+    private DistributionCenter distributionCenter;
+    private LocalDateTime sendDate;
+
+    public Donation(DistributionCenter distributionCenter, LocalDateTime sendDate) {
+        this.distributionCenter = distributionCenter;
+        this.sendDate = sendDate;
+    }
+}
