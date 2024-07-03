18ac4ede8734d0883a637bc21fe0a45204a5fd1f
Paulo Cesar <paulocesarpinheirodasilva0@gmail.com>
2024-07-03 10:56:10 -0300

    feat: add Food entity

---
+++ src/main/java/com/paulocesar/entity/Food.java
@@ -0,0 +1,23 @@
+package com.paulocesar.entity;
+
+import com.paulocesar.entity.enums.ItemType;
+import com.paulocesar.entity.enums.UnitMeasurement;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+public class Food extends Item {
+    private Double quantity;
+    private UnitMeasurement unitMeasurement;
+    private LocalDate expirationDate;
+
+    public Food(String itemName, Double quantity, UnitMeasurement unitMeasurement, LocalDate expirationDate) {
+        super(ItemType.FOODS, itemName);
+        this.quantity = quantity;
+        this.unitMeasurement = unitMeasurement;
+        this.expirationDate = expirationDate;
+    }
+}
