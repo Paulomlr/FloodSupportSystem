1346c80ed95f97c384a44bd72f1fd5ccee6cf1a4
Paulo Cesar <paulocesarpinheirodasilva0@gmail.com>
2024-07-14 00:03:20 -0300

    feat: Add interactive loop in Main for user interaction

--- src/main/java/com/paulocesar/Main.java
+++ src/main/java/com/paulocesar/Main.java
@@ -1,9 +1,22 @@
 package com.paulocesar;
 
+import com.paulocesar.entity.DistributionCenter;
+import com.paulocesar.entity.Shelter;
+import com.paulocesar.services.DistributionCenterService;
+import com.paulocesar.services.ShelterService;
 import com.paulocesar.util.*;
 
+import java.util.List;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class Main {
-    public static void main(String[] args) {
+    private static final Logger logger = Logger.getLogger(Main.class.getName());
+    private static final DistributionCenterService centerService = new DistributionCenterService();
+    private static final ShelterService shelterService = new ShelterService();
+
+    public static void loadCsvData(){
         DistributionCenterCSVReader centerCSVReader = new DistributionCenterCSVReader();
         DonationCSVReader donationCSVReader = new DonationCSVReader();
         ItemCSVReader itemCSVReader = new ItemCSVReader();
@@ -19,5 +32,60 @@ public class Main {
         shelterCSVReader.readerCSVFile();
         orderCSVReader.readerCSVFile();
         orderItemCSVReader.readerCSVFile();
+    }
+
+    public static void main(String[] args) {
+        Logger.getLogger("org.hibernate").setLevel(Level.SEVERE);
+        logger.info("Starting the application...");
+
+        loadCsvData();
+        Scanner in = new Scanner(System.in);
+        boolean exit = false;
+
+        while (!exit) {
+            displayMenu();
+            int option = in.nextInt();
+
+            switch (option){
+                case 1 -> {
+                    System.out.println();
+                    listDistributionCenter();
+                    System.out.println();
+                }
+                case 2 -> {
+                    System.out.println();
+                    listShelters();
+                    System.out.println();
+                }
+                case 3 -> exit = true;
+            }
+        }
+        centerService.close();
+        logger.info("Finishing the application...");
+    }
+
+    public static void displayMenu(){
+        System.out.println("1. List Distribution Centers");
+        System.out.println("2. List Shelters");
+        System.out.println("3. Exit");
+        System.out.print("Choose an option: ");
+    }
+
+    private static void listDistributionCenter() {
+        System.out.println("----------DISTRIBUTION CENTERS----------");
+        List<DistributionCenter> centers = centerService.getAllDistributionCenter();
+
+        for(DistributionCenter center : centers){
+            System.out.println(center.getId() + ": " + center.getName());
+        }
+    }
+
+    private static void listShelters() {
+        System.out.println("----------SHELTERS----------");
+        List<Shelter> shelters = shelterService.getAllShelter();
+
+        for(Shelter shelter: shelters){
+            System.out.println(shelter.getId() + ": " + shelter.getName());
+        }
     }
 }
